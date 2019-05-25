092ab510af47f587724867071830a9993db6a0d6
Rammina <altinaschwarzer@gmail.com>
2019-05-26 07:19:41 +0800

    added navbar skeleton

--- main/js/main.js
+++ main/js/main.js
@@ -1,4 +1,44 @@
-// random comment
 
+// document objects
 
+// navigation bar objects
+
+var navbar = {
+	header: document.querySelector(".main-header"),
+	name: document.querySelector("#nav__name"),
+	menubutton: document.querySelector(".nav__menu"),
+	menuitemscontainer: document.querySelector(".nav__items-container"),
+	menuitems: document.querySelector(".nav__items"),
+	menuitem: document.querySelectorAll(".nav__item"),
+	showMenu: function showMenu() {
+		navbar.menuitemscontainer.classList.add("show")
+		navbar.menubutton.setAttribute("aria-expanded", "true");
+	},
+	hideMenu: function hideMenu() {
+		navbar.menuitemscontainer.classList.remove("show")
+		navbar.menubutton.setAttribute("aria-expanded", "false");
+	},
+	menutouched: false
+}
+
+navbar.menubutton.addEventListener("touchstart", function(){
+	if(navbar.menuitemscontainer.classList.contains("show")) {
+		navbar.hideMenu();
+		navbar.menutouched = true;
+	}
+	else if(!(navbar.menuitemscontainer.classList.contains("show"))) {
+		navbar.showMenu();
+		navbar.menutouched = true;
+	}
+});
+
+navbar.menubutton.addEventListener("click", function(){
+	if(navbar.menuitemscontainer.classList.contains("show") && (navbar.menutouched === false)) {
+		navbar.hideMenu();
+	}
+	else if(!(navbar.menuitemscontainer.classList.contains("show")) && (navbar.menutouched === false)) {
+		navbar.showMenu();
+	}
+	navbar.menutouched = false;
+});
 
